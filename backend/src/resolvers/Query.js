60f10f69036a0ab27f6314a3601858acb4eb62a8
natnaelawel <natnael.awel@gmail.com>
2020-10-14 07:40:59 +0300

    server side graphql setup

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+/// if there is no filtering or any authentication 
+const {forwardTo} = require('prisma-binding')
+
+const Query = {
+    items: forwardTo('db')
+    // async items(parent, args, ctx, info){
+    //     const items = await ctx.db.query.items();
+    //     return items; 
+    // }
+};
 
 module.exports = Query;
